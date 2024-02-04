effc80084e8d980a9c50c99c09e2334a564a80fb
-MADUKA- <w.madhuka@gmail.com>
2024-02-04 13:08:20 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
       <div className="TopSection">
         <div className="HeroImage">
        <img
-            src={window.innerWidth >= 750 ? header : mobileHeader}
+            src={window.innerWidth >= 500 ? header : mobileHeader}
             alt="Ayuhasca Hero"
           />
         </div>
