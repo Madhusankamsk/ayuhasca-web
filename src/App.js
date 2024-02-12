dae541d4809dc7e1b2e3a8fb25cac4fb284ae532
-MADUKA- <w.madhuka@gmail.com>
2024-02-12 19:20:46 +0530

    UI updates responsiveness

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
       <div className="TopSection">
         <div className="HeroImage">
        <img
-            src={window.innerWidth >= 500 ? header : mobileHeader}
+            src={window.innerWidth >= 320 ? header : mobileHeader}
             alt="Ayuhasca Hero"
           />
         </div>
